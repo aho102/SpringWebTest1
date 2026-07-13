30a5653a95a03dc71dde5687b443bfbbb61fce79
aho102 <maharjanmamika@gmail.com>
2026-07-13 07:36:59 +0545

    Initial commit

---
+++ src/main/java/io/virinchi/springwebtest1/SpringWebTest1Application.java
@@ -0,0 +1,13 @@
+package io.virinchi.springwebtest1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebTest1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebTest1Application.class, args);
+    }
+
+}
