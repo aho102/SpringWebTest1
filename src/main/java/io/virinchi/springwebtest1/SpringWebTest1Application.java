5fc68c17ba418a9d32443d8f7ef530b35fbbbcdb
aho102 <maharjanmamika@gmail.com>
2026-07-13 08:08:24 +0545

    13th july

--- src/main/java/io/virinchi/springwebtest1/SpringWebTest1Application.java
+++ src/main/java/io/virinchi/springwebtest1/SpringWebTest1Application.java
@@ -5,11 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringWebTest1Application {
-
-
-
-
-
     public static void main(String[] args) {
         SpringApplication.run(SpringWebTest1Application.class, args);
     }
