32d550766537c32e9f915f9d3eb97395f21a46d6
aho102 <maharjanmamika@gmail.com>
2026-08-06 08:00:03 +0545

    6th aug

--- src/main/java/io/virinchi/springwebtest1/Controller/SignupLoginController.java
+++ src/main/java/io/virinchi/springwebtest1/Controller/SignupLoginController.java
@@ -1,12 +1,17 @@
 package io.virinchi.springwebtest1.Controller;
 
+import io.virinchi.springwebtest1.Model.UserTbl;
+import io.virinchi.springwebtest1.Repository.UserRepository;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class SignupLoginController {
+    @Autowired // only necessary function are taken and hides unwanted function
+    private UserRepository uRepo; // making object of UserRepository(interface) // this object is use for database
     @GetMapping("/signup")
     public String signup() {
         return "signupPage";
@@ -24,8 +29,14 @@ public class SignupLoginController {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
 
-        System.out.println(username);
-        System.out.println(password);
+        UserTbl user = new UserTbl();
+        user.setUsername(username);
+        user.setPassword(password);
+
+        uRepo.save(user);
+        // save -> curd relation
+        //one row at a time
+
 
         return "loginPage";
     }
