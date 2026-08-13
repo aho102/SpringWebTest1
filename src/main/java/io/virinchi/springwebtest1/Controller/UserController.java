c916be0af77b4595ca7a376397112573aadb14ba
aho102 <maharjanmamika@gmail.com>
2026-08-13 21:13:57 +0545

    13th aug home.html and  user controller delete

--- src/main/java/io/virinchi/springwebtest1/Controller/UserController.java
+++ src/main/java/io/virinchi/springwebtest1/Controller/UserController.java
@@ -1,7 +1,6 @@
 package io.virinchi.springwebtest1.Controller;
 
 import io.virinchi.springwebtest1.Repository.UserRepository;
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,15 +11,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class UserController {
 
     @Autowired
-    private UserRepository uRepo; // no need to use new keyword also know as dependency
+    private UserRepository uRepo; // no need to use new keyword also known as dependency
 
 
     @PostMapping("/deleteUser")
      public String  deletePost(@RequestParam("id") int id, Model m){
     //int id = req.getParameter("id");  // req.getParameter ley string value falxa yesma pheri change gari rakhnu parxa
         uRepo.deleteById(id);
-        m.addAttribute("totalUser",uRepo.findAll());
-        return ("home");
+        m.addAttribute("totalUsers",uRepo.findAll());
+        return "home";
     }
 
 }
