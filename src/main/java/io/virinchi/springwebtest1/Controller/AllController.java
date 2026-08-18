576eaff1e61360513f855ae643f8207735057b66
aho102 <maharjanmamika@gmail.com>
2026-08-18 21:29:07 +0545

    17 Aug, editpage(created) and gallerypage (changes), Allcontroller and Usercontroller also in model ImgTable created

--- src/main/java/io/virinchi/springwebtest1/Controller/AllController.java
+++ src/main/java/io/virinchi/springwebtest1/Controller/AllController.java
@@ -1,12 +1,19 @@
 package io.virinchi.springwebtest1.Controller;
 
 
+
+import io.virinchi.springwebtest1.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 //controller msanage http request: get  mapping and post mapping , etc // helps get and post mapping
 public class AllController {
+
+    @Autowired
+    private UserRepository uRepo;
 
     @GetMapping("/")
 public String firstPage(){ //function name can be anything
@@ -14,7 +21,11 @@ public String firstPage(){ //function name can be anything
 }
 
 @GetMapping("/home")
-    public String homeGet(){
-        return "home.html";
+public String homeGet(Model m)
+{
+    m.addAttribute("totalUsers",uRepo.findAll() );
+
+    return "home.html";
+
 }
 }
--- src/main/java/io/virinchi/springwebtest1/Controller/UserController.java
+++ src/main/java/io/virinchi/springwebtest1/Controller/UserController.java
@@ -1,9 +1,12 @@
 package io.virinchi.springwebtest1.Controller;
 
+
+import io.virinchi.springwebtest1.Model.UserTbl;
 import io.virinchi.springwebtest1.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -18,6 +21,32 @@ public class UserController {
      public String  deletePost(@RequestParam("id") int id, Model m){
     //int id = req.getParameter("id");  // req.getParameter ley string value falxa yesma pheri change gari rakhnu parxa
         uRepo.deleteById(id);
+        m.addAttribute("totalUsers",uRepo.findAll());
+        return "home";
+    }
+
+    @PostMapping("/editUser")
+    public String editUser(@RequestParam("id") int id ,Model m )
+    {
+        UserTbl user = uRepo.getById(id);
+// user ko information -> id, username, password
+        //user.getId()......etc
+
+        m.addAttribute("user",user);
+        return "editPage";
+    }
+
+    @PostMapping("/updateUser")
+    public String updateUser(@ModelAttribute UserTbl user , Model m)
+    {
+        //@ModelAttribute-> if the parameters like id,username,password matches
+        //exactly with the model's table.
+        //IN our case, our model UserTbl also has id, username,password that is coming
+        //exactly from editPage.html, we use @ModelAttribute
+
+        uRepo.save(user);
+        //It uses the id parameter to update its username, we can directly use uRepo.save function
+
         m.addAttribute("totalUsers",uRepo.findAll());
         return "home";
     }
